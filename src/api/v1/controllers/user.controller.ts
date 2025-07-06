724a14c25a5e6edc808820af2b93d8cfbd43b2b0
Pethum Jeewantha <pethumj@balnzed.com>
2025-07-06 12:32:09 +0530

    update pw

--- src/api/v1/controllers/user.controller.ts
+++ src/api/v1/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import client from "../../../services/database/prisma.service";
 import logger from "../../../services/common/logger.service";
+import bcrypt from "bcrypt";
 
 export default class UserController {
 
@@ -34,4 +35,50 @@ export default class UserController {
             res.status(500).json({ error: 'Internal Server Error' });
         }
     }
+
+    public async updateUser(req: Request, res: Response): Promise<any> {
+        try {
+            const userId = req.user!.uid;
+            const { name, email, walletAddress, password } = req.body;
+
+            if (!name && !email && !walletAddress && !password) {
+                return res.status(400).json({
+                    error: 'At least one field (name, email, walletAddress) must be provided for update'
+                });
+            }
+
+            const updateData: any = {};
+            if (name) updateData.name = name;
+            if (email) updateData.email = email;
+            if (walletAddress) updateData.walletAddress = walletAddress;
+            if (password) updateData.password = await bcrypt.hash(password, 10);
+
+            if (email) {
+                const existingUser = await client.prisma.user.findUnique({
+                    where: { email }
+                });
+
+                if (existingUser && existingUser.id !== userId) {
+                    return res.status(409).json({
+                        error: 'Email already exists'
+                    });
+                }
+            }
+
+            const updatedUser = await client.prisma.user.update({
+                where: { id: userId },
+                data: updateData
+            });
+
+            const { password: _, ...userWithoutPassword } = updatedUser;
+            return res.status(200).json({
+                message: "User updated successfully",
+                user: userWithoutPassword,
+            });
+        } catch (error) {
+            logger.error('Error updating user', { error });
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
 }
--- src/api/v1/routes/user.route.ts
+++ src/api/v1/routes/user.route.ts
@@ -5,6 +5,7 @@ import UserController from "../controllers/user.controller";
 const userRoute: Router = Router();
 const controller = new UserController()
 
+userRoute.put("/", controller.updateUser);
 userRoute.post("/initial-enable", controller.initialEnable);
 
 export default userRoute;
